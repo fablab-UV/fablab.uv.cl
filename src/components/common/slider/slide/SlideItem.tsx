045f87935e5a4355f551b41d53a2910ba537d4b9
ixyzdev <ixyz.dev@gmail.com>
2025-03-28 20:10:25 -0300

    fix(slider): solucionando traslucencia para todas los usos

--- src/components/common/slider/slide/SlideItem.tsx
+++ src/components/common/slider/slide/SlideItem.tsx
@@ -20,10 +20,11 @@ const SlideItem: React.FC<Slide> = ({ slide }) => {
           className="object-cover object-center"
         />
 
+        <div className="absolute inset-0 bg-gradient-to-t from-[#100e09] via-transparent to-transparent"></div>
+
         {/* Overlay for the image */}
         {slide.title != null && slide.description != null ? (
           <section className="absolute inset-0 hidden size-full flex-col justify-end p-4 md:flex">
-            <div className="absolute inset-0 bg-gradient-to-t from-[#100e09] via-transparent to-transparent"></div>
             <div className="relative z-10 mx-32">
               <div className="w-[65vh] rounded-lg bg-white px-10 py-2 shadow-lg transition-transform duration-300 hover:scale-105">
                 <h1 className="my-5 text-4xl font-bold">{slide.title}</h1>
