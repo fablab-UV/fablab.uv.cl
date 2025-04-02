a7015d725ea64a6a643f98e2ab83ce47528ad39b
Giovvni <giovanni.vasquez@alumnos.uv.cl>
2025-04-02 01:01:56 -0300

    fix: desktop navbar fix e implementacion movil

--- src/layouts/main-layout/components/navbar/NavBar.tsx
+++ src/layouts/main-layout/components/navbar/NavBar.tsx
@@ -31,7 +31,9 @@ const NavBar: React.FC = () => {
         {/* Botón de hamburguesa */}
         <button
           className="ml-auto flex flex-col items-center justify-center gap-1 rounded-md border-none p-2 md:hidden"
-          onClick={() => { setIsMenuOpen(!isMenuOpen); }}
+          onClick={() => {
+            setIsMenuOpen(!isMenuOpen)
+          }}
         >
           <div className="h-0.5 w-6 bg-white"></div>
           <div className="h-0.5 w-6 bg-white"></div>
@@ -43,7 +45,9 @@ const NavBar: React.FC = () => {
           {menuItems.map((item) => (
             <div
               key={item.name}
-              onMouseEnter={() => { handleMouseEnter(item.name); }}
+              onMouseEnter={() => {
+                handleMouseEnter(item.name)
+              }}
               onMouseLeave={handleMouseLeave}
               className="group relative"
             >
@@ -55,30 +59,29 @@ const NavBar: React.FC = () => {
                 </Button>
               </Link>
 
+              {item.subItems != null && hoveredItem === item.name && (
+                <>
+                  {/* Barra blanca que cubre toda la pantalla y se alinea con los subitems */}
+                  <div
+                    className="fixed left-0 z-40 hidden w-screen bg-white transition-all duration-200 ease-in-out md:block"
+                    style={{
+                      top: '260px',
+                      height: `${item.subItems.length * 60}px`
+                    }}
+                  ></div>
 
-              {(item.subItems != null) && hoveredItem === item.name && (
-  <>
-                {/* Barra blanca que cubre toda la pantalla y se alinea con los subitems */}
-                <div 
-                  className="fixed left-0 z-40 hidden w-screen bg-white transition-all duration-200 ease-in-out md:block"
-                  style={{ top: "260px", height: `${item.subItems.length * 60}px` }}
-                ></div>
-
-                {/* Contenedor de los subitems */}
-                <div className="absolute left-0 top-full z-50 mt-3 hidden w-screen bg-white md:block">
-                  {item.subItems.map((subItem) => (
-                    <Link key={subItem.name} href={subItem.href}>
-                      <div className="cursor-pointer  p-2 py-4 text-left text-sm text-[#210a3e] hover:bg-gray-100">
-                        {subItem.name.toUpperCase()}
-                      </div>
-                    </Link>
-                  ))}
-                </div>
-              </>
-            )}
-
-
-              
+                  {/* Contenedor de los subitems */}
+                  <div className="absolute left-0 top-full z-50 mt-3 hidden w-screen bg-white md:block">
+                    {item.subItems.map((subItem) => (
+                      <Link key={subItem.name} href={subItem.href}>
+                        <div className="cursor-pointer p-2 py-4 text-left text-sm text-[#210a3e] hover:bg-gray-100">
+                          {subItem.name.toUpperCase()}
+                        </div>
+                      </Link>
+                    ))}
+                  </div>
+                </>
+              )}
             </div>
           ))}
         </div>
@@ -94,7 +97,7 @@ const NavBar: React.FC = () => {
                 </div>
               </Link>
               {/* Submenú en mobile */}
-              {(item.subItems != null) && (
+              {item.subItems != null && (
                 <div className="flex flex-col bg-gray-200">
                   {item.subItems.map((subItem) => (
                     <Link key={subItem.name} href={subItem.href}>
@@ -107,10 +110,8 @@ const NavBar: React.FC = () => {
               )}
             </div>
           ))}
-          <div className='h-px bg-[#210a3e]'>    
-          </div>
+          <div className="h-px bg-[#210a3e]"></div>
         </div>
-    
       )}
     </nav>
   )
--- src/layouts/regular-layout/RegularLayout.tsx
+++ src/layouts/regular-layout/RegularLayout.tsx
@@ -16,7 +16,7 @@ interface RootLayoutProps {
 const RegularLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <>
-      <header className=" md:block">
+      <header className="md:block">
         <TopBar />
         <NavBar />
       </header>
